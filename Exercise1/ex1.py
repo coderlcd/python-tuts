8eb820fcdc652e9c9aeaad5e4bf41ce78f21c718
CodeRed <codered@CodeReds-iMac.local>
2017-02-25 18:07:45 -0600

    Completed Ex1

---
+++ Exercise1/ex1.py
@@ -0,0 +1,8 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+#print "I'd much rather you 'not'." #The octothorpe comments out code
+print 'I "said" do not touch this.'
+print "I am able to print to the screen! Woo!"
