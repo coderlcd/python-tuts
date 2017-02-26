9f34eb9cab187ac5979e389abaef2e653ab59bfc
CodeRed <codered@CodeReds-iMac.local>
2017-02-25 18:37:32 -0600

    Completed Ex3 Study Drills

--- Exercise3/ex3.py
+++ Exercise3/ex3.py
@@ -1,23 +1,37 @@
+#Outputs a string
 print "I will now count my chickens:"
 
-print "Hens", 25 + 30 / 6
-print "Roosters", 100 - 25 * 3 % 4
+#Outputs a string, divides 30 by 6, then adds 25
+print "Hens", 25.0 + 30.0 / 6.0
+#Outputs a string, 25 times 3, then mods that by 4, then subtracts that from 100
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
+#Outputs a string
 print "Now I will count the eggs:"
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#Outputs calculation, mod first, then division, then add/sub left to right
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
+#Outputs a string
 print "Is it true that 3 + 2 < 5 - 7?"
 
-print 3 + 2 < 5 - 7
+#Outputs TRUE/FALSE based on calculation (FALSE)
+print 3.0 + 2.0 < 5.0 - 7.0
 
-print "What is 3 + 2?", 3 + 2
-print "What is 5 - 7?", 5 - 7
+#Outputs a string, followed by the addition of 3 plus 2
+print "What is 3 + 2?", 3.0 + 2.0
+#Outputs a string, followed by 5 minus 7
+print "What is 5 - 7?", 5.0 - 7.0
 
+#Outputs a string
 print "Oh, that's why it's False."
 
+#Outputs a string
 print "How about some more."
 
-print "Is it greater?", 5 > -2
-print "Is it greater or equal?", 5 >= -2
-print "Is it less or equal?", 5 <= -2
+#Outputs a string, followed by TRUE/FALSE (TRUE)
+print "Is it greater?", 5.0 > -2.0
+#Outputs a string, followed by TRUE/FALSE (TRUE)
+print "Is it greater or equal?", 5.0 >= -2.0
+#Outputs a string, followed by TRUE/FALSE (FALSE)
+print "Is it less or equal?", 5.0 <= -2.0
---
+++ Exercise3/studydrill_ex3.py
@@ -0,0 +1,5 @@
+#Study Drill - Exercise 3
+#Complete a calculation utilizing Python
+
+print "What is the solution to 6.0 % 5.0 / 4.0 * 3.0 - 2.0 % 1.0 * 9.0 + 0.0"
+print 6.0 % 5.0 / 4.0 * 3.0 - 2.0 % 1.0 * 9.0 + 0.0
